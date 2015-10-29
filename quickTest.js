bc8fbfff22207cd015717b8c63a680393a498855
Alexis Tessier <alexis.tessier@designdo.fr>
2015-10-30 00:56:00 +0100

    quick test

--- quickTest.js
+++ quickTest.js
@@ -1,5 +1,17 @@
-/*about(typeof 20)
-	.equal('number')
-.assert();*/
+var aboutTypeOf = assertAbout.createPlugin("url");
 
-console.log(global);
+aboutTypeOf.defineAssertion("is", function(about, topic, type) {
+    about.string(topic)
+        .contains("http")
+        .greaterThan(6)
+        (function(resolve, reject) {
+        	request(topic)
+        		.then(function() {
+        			about.number(duration)
+        				.lowerThan(500)
+        			.assert().then(resolve)
+        		})
+        		.catch(reject);
+        })
+    .assert()
+});
